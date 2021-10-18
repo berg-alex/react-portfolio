c24265aecef1a8c02dbc789fe7e3d468728cfc86
Alex <abergsf2017@gmail.com>
2021-10-18 15:08:07 -0700

    added more to map description

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,16 @@ import Mymap from './Components/gmap-circles';
 function App() {
   
   return (
-    <div>
+    <div className="app">
       <Mynav />
-      <div className="map-title">Map marked with areas I would consider relocating to, with preference for remote roles.</div>
+      <div className="map-title">
+        Map marked with areas I would consider relocating to, 
+        with preference for remote roles.
+        <br/>
+        Open to freelance, contract, direct hire, part-time, full-time.
+
+        Eligible to work in US & UK.
+      </div>
       <Mymap />
       {/* { initMap } */}
   </div>
